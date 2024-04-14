8ae9d9efce4ec3f4399588bbd83597416b49b83b
Kuroi_Cc <chenchengmikuro@gmail.com>
2024-04-14 17:59:13 +0900

    ci: :hammer: Add root_path configuration option to start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
+export COUNT_POINT_ROOT_PATH=/mahjong_record_counter
+
 `nohup poetry run start >> $PROJECTS/mahjong-record-counter-python/mahjong_record_counter/logs/server.log 2>&1 &` ; echo $!
